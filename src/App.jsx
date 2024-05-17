246b0321087f57c12bc64bbb83b45b74642f465f
abel3ri <abelmerete@yahoo.com>
2024-05-17 20:16:26 +0300

    install axios pkg

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Counter from "./components/Counter";
+import Pricing from "./components/Pricing";
 
 function App() {
   return (
     <>
       <Counter />
+      {/* <Pricing /> */}
     </>
   );
 }
